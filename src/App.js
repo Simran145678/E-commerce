c8802a773baf23d57b6c8e0a200b767f13fcec94
HarsimranKaur1997 <hkaur607@my.centennialcollege.ca>
2021-09-02 19:11:34 -0400

    adressform and paymentform added

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ const App = () => {
             />
           </Route>
           <Route exact path="/checkout">
-            <Checkout />
+            <Checkout cart={cart} />
           </Route>
         </Switch>
       </div>
--- src/components/CheckoutForm/CustomTextField.jsx
+++ src/components/CheckoutForm/CustomTextField.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { useFormContext, Controller } from "react-hook-form";
-import { TextField, Grid } from "@material-ui/core";
 
-const FormInput = ({ name, label, required }) => {
+import { TextField, Grid } from "@material-ui/core";
+import { useFormContext, Controller } from "react-hook-form";
+
+function FormInput({ name, label }) {
   const { control } = useFormContext();
+
   return (
     <Grid item xs={12} sm={6}>
       <Controller
@@ -12,10 +14,11 @@ const FormInput = ({ name, label, required }) => {
         control={control}
         label={label}
         fullWidth
-        required={required}
+        required
+        render={({ field }) => <TextField fullWidth label={label} required />}
       />
     </Grid>
   );
-};
+}
 
 export default FormInput;
