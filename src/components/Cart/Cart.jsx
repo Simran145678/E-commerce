2845b2cc5bb151efadc8bf124d9c4da069c6197f
HarsimranKaur1997 <hkaur607@my.centennialcollege.ca>
2021-09-02 15:59:10 -0400

    checkout forms components setted up

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,13 +2,22 @@ import React from "react";
 import { Container, Typography, Button, Grid } from "@material-ui/core";
 import useStyles from "./styles";
 import CartItem from "./CartItem/CartItem";
+import { Link } from "react-router-dom";
 
-const Cart = ({ cart }) => {
+const Cart = ({
+  cart,
+  handleUpdateCartQty,
+  handleRemoveFromCart,
+  handleEmptyCart,
+}) => {
   const classes = useStyles();
 
   const EmptyCart = () => (
     <Typography variant="subtitle1">
-      You have no item in your shopping cart, start adding some!
+      You have no item in your shopping cart.
+      <Link to="/" className={classes.link}>
+        start adding some!
+      </Link>
     </Typography>
   );
 
@@ -17,7 +26,11 @@ const Cart = ({ cart }) => {
       <Grid container spacing={3}>
         {cart.line_items.map((item) => (
           <Grid item xs={12} sm={4} key={item.id}>
-            <CartItem item={item} />
+            <CartItem
+              item={item}
+              handleUpdateCartQty={handleUpdateCartQty}
+              handleRemoveFromCart={handleRemoveFromCart}
+            />
           </Grid>
         ))}
       </Grid>
@@ -32,10 +45,13 @@ const Cart = ({ cart }) => {
             type="button"
             variant="contained"
             color="secondary"
+            onClick={handleEmptyCart}
           >
             Empty Cart
           </Button>
           <Button
+            component={Link}
+            to="/checkout"
             className={classes.checkoutButton}
             size="large"
             type="button"
---
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PaymentForm = () => {
+  return <div>Paymentform</div>;
+};
+
+export default PaymentForm;
